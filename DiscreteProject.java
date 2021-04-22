988066380338516a39fe45884c2787ff19144c07
sujinkim1225 <68390111+sujinkim1225@users.noreply.github.com>
2021-04-22 15:16:38 -0700

    Add files via upload
    Hello everyone, 
    My name is Sujin Kim.
    
    I've started working on my project for the class, and I just wanted to share my topic and progress with the class.
    I'm currently in the process of making a simple "secret message translator" program (in Java) that can encrypt and decrypt messages the user inputs using the Caesar cipher we've learned in chapter 4.
    My program works fine as of now but, I'm planning to refine/improve some of my codes in the next few days and also make ppt slides explaining Caesar ciphers and my codes in detail.
    
    Feel free to comment/give feedback on my work and I'll keep you guys posted with my progress.

---
+++ DiscreteProject.java
@@ -0,0 +1,120 @@
+//Sujin Kim
+//Discrete Math Project
+//Title: Secret Message Translator
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discrete.project;
+
+import java.util.Scanner; //Need for Scanner class
+/**
+ *
+ * @author Admin
+ */
+public class DiscreteProject 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int mainChoice;
+        final int SHIFT = 3;
+        String text, cipher;
+        
+       Scanner keyboard = new Scanner (System.in);
+       
+       do{
+           
+       displayMainMenu();
+       
+       mainChoice = keyboard.nextInt();// TODO code application logic here
+        keyboard.nextLine(); //returns true if and only if this scanner has another line of input
+       
+       switch(mainChoice)
+		{
+                  case 1:
+                        System.out.print("\nEnter The Message to EnCrypt: ");
+                        text = keyboard.nextLine();
+                        String encryptedMsg = encryptMessage(text,SHIFT).toString();
+                        System.out.println("Secrect Message: " + encryptedMsg);
+                    break;
+                      
+                  case 2:
+                        System.out.println("\nEnter The Message to Decrypt: ");
+                        cipher = keyboard.nextLine();
+                        String decryptedMsg =decryptMessage(cipher,26-SHIFT).toString();
+			System.out.println("Decrypted Mssage: " + decryptedMsg);//Decrypt Message
+                    break;
+                      
+                  case 3:
+                    
+                    break;
+                }
+        }while (mainChoice != 3);
+    }
+    
+    //displayMainMenu method displays main menu options
+	public static void displayMainMenu()     
+	{      
+                System.out.println("=================================================================================");
+                System.out.println("|Sujin's Secret Message Translator| ");
+                System.out.println("-This Program will encrypt and decrypt message by using Caesar Cipher technique-");
+		System.out.println("1. Encrypt Message");
+		System.out.println("2. Decrypt Message");
+                System.out.println("3. Exit the Program");
+                System.out.print("Press 1,2 or 3 to Exit the Program: ");
+		
+	}
+        
+        public static StringBuffer encryptMessage(String text, int s)
+            {
+                 StringBuffer result= new StringBuffer();
+
+            for (int i=0; i<text.length(); i++)
+            {
+                if (Character.isUpperCase(text.charAt(i)))
+                {
+                    char ch = (char)(((int)text.charAt(i) +
+                                      s - 65) % 26 + 65);
+                    result.append(ch);
+                }
+                else
+                {
+                    char ch = (char)(((int)text.charAt(i) +
+                                      s - 97) % 26 + 97);
+                    result.append(ch);
+                }
+
+            }
+            return result;
+        }
+        
+        public static StringBuffer decryptMessage(String cipher, int s)
+        {
+        
+             StringBuffer result= new StringBuffer();
+
+            for (int i=0; i<cipher.length(); i++)
+            {
+                if (Character.isUpperCase(cipher.charAt(i)))
+                {
+                    char ch = (char)(((int)cipher.charAt(i) +
+                                      s - 65) % 26 + 65);
+                    result.append(ch);
+                }
+                else
+                {
+                    char ch = (char)(((int)cipher.charAt(i) +
+                                      s - 97) % 26 + 97);
+                    result.append(ch);
+                }
+
+            }
+            return result;
+        }
+}
