3fe6f55c68ea07c53655fb57f69e2d853a7ea695
j-reyes27 <81289457+j-reyes27@users.noreply.github.com>
2021-04-29 16:48:37 -0700

    Update Original Hash Function.cpp
    This is a slightly different algorithm from the original and gets completely new hashes. I also fixed an input issue as well as added a "step-by-step" showcase of my hash function.

--- Original Hash Function.cpp
+++ Original Hash Function.cpp
@@ -136,21 +136,83 @@ string myHash(string text)                                      //uses previous 
 {
     int t = addASCII(text);                                     //adds up the ASCII values of the string's characters
     string gB1 = padBin(stringtoBinary(text));                  //gets the binary value of the string padded to a multiple of 32
+    int blength = gB1.length();                                 //gets the binary length prior to compression
 
     if (gB1.length() > 32) gB1 = compressBin(gB1);              //if the binary has more than 32 bits (64, 96, 128, etc), then it uses my compression function
 
     string gB2 = gB1 + "";                                      //assigns the original binary value to another string called gB2
     string gB3 = gB1 + "";                                      //assigns the original binary value to another string called gB3
 
-    rightRotate(gB2, gB2.length() % 3);                         //rotates gB2 by its length % 3
+    rightRotate(gB2, blength % 3);                              //rotates gB2 by its length % 3
     rightRotate(gB2, t % 5);                                    //rotates gB2 by its ASCII total % 5
-    leftRotate(gB3, gB3.length() % 7);                          //rotates gB3 by its length % 7
+    leftRotate(gB3, blength % 7);                               //rotates gB3 by its length % 7
     leftRotate(gB3, t % 11);                                    //rotates gB3 by its ASCII total % 11           *These values are the first 4 prime numbers following 2
 
     string gB4 = XOR(gB2, gB3, gB1.length());                   //creates a new binary value that is gB2 XOR gB3 = gB4
     string gB5 = XOR(gB1, gB4, gB4.length());                   //creates a new binary value that is gB4 XOR gB1 = gB5
 
     string hash = binToHex(gB5);                                //converts the gB5 binary to hexadecimal, which is the final hash returned
+
+    return hash;
+}
+
+string cmyHash(string text)                                     //this is the same hashing algorithm but it prints out how most of the steps work
+{
+    cout << "Original Text: " << text << "\n";
+
+    int t = addASCII(text);                                     
+
+    cout << "First, get total ASCII value: " << t << "\n";
+
+    string gB1 = padBin(stringtoBinary(text));
+    int blength = gB1.length();
+
+    cout << "Next, get the padded binary value of the original text which is stored as gB1: " << gB1 << "\n";
+    cout << "Store the length of this binary for later use: " << blength << "\n";
+
+    if (gB1.length() > 32) gB1 = compressBin(gB1);             
+
+    cout << "If gB1 has more than 32 bits, then compress to 32 and use for gB2 and gB3:                 " << gB1 << "\n";
+
+    string gB2 = gB1 + "";                                     
+    string gB3 = gB1 + "";                                      
+
+    rightRotate(gB2, blength % 3);
+
+    cout << "\ngB2: \n";
+    cout << "Rotate by the binary length mod 3 which in this case is " << blength % 3 << " times to the right:              " << gB2 << "\n";
+
+    rightRotate(gB2, t % 5);                                 
+
+    cout << "Rotate the same thing by the total ASCII mod 5 which in this case is " << t % 5 << " times to the right: " << gB2 << "\n\n";
+
+    leftRotate(gB3, blength % 7);
+
+    cout << "\ngB3: \n";
+    cout << "Rotate by the binary length mod 7 which in this case is " << blength % 7 << " times to the left:               " << gB3 << "\n";
+
+    leftRotate(gB3, t % 11);                                   
+
+    cout << "Rotate the same thing by the total ASCII mod 11 which in this case is " << t % 11 << " times to the left: " << gB3 << "\n\n";
+
+    cout << "Now do XOR for gB2 and gB3 which gets us gB4: \n";
+    cout << "    " << gB2 << "\n";
+    cout << "XOR " << gB3 << "\n";
+
+    string gB4 = XOR(gB2, gB3, gB1.length());                   
+
+    cout << "    " << gB4 << "\n\n";
+    cout << "Now do XOR for gB4 and gB1 which gets us gB5: \n";
+    cout << "    " << gB4 << "\n";
+    cout << "XOR " << gB1 << "\n";
+
+    string gB5 = XOR(gB1, gB4, gB4.length());    
+
+    cout << "    " << gB5 << "\n\n";
+
+    string hash = binToHex(gB5);                             
+
+    cout << "This gB5 value is converted to hexadecimal to get the final hash digest: ";
 
     return hash;
 }
@@ -165,14 +227,21 @@ int main()
     string a = "";
 
     cout << "Test Cases From Main:\n";
-    cout << myHash(g) << "\n";                                  //Result should be C56244CD
-    cout << myHash(h) << "\n";                                  //Result should be 47C0C7B1
-    cout << myHash(i) << "\n";                                  //Result should be 4251A8C1
-    cout << myHash(j) << "\n";                                  //Result should be E19D3F81
-    cout << myHash(k) << "\n";                                  //Result should be E909D216
+    cout << myHash(g) << "\n";                                  
+    cout << myHash(h) << "\n";                                  
+    cout << myHash(i) << "\n";                                 
+    cout << myHash(j) << "\n";                                  
+    cout << myHash(k) << "\n";                                  
     cout << "\n";
 
-    cout << "Enter your own message to be hashed: ";
+    /*  Results for the test cases: 
+        15B05E94
+        CCE51043
+        12285508
+        91A4960B
+        5D7C7AD0                        */
+
+    cout << "Enter your own message to see how the hashing function works: ";
     getline(cin >> ws, a);
-    cout << "Hash: " << myHash(a) << "\n";
+    cout << "\n" << cmyHash(a) << "\n";
 }
