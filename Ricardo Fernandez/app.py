4266fbcdff83f4d019ca79a7449a0744a9590826
Ricardo Fernandez <ric2fer2@gmail.com>
2021-05-16 02:59:24 -0700

    updated var names and input option

--- Ricardo Fernandez/app.py
+++ Ricardo Fernandez/app.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 #The sides of our dice
 dice= [1,2,3,4,5,6]
 #number of dice rolled
@@ -7,25 +8,26 @@ n = 2
 #desired sum from dice rolls
 desiredSum = 6
 
-#creates a list of all the possible permutations of two fair dice rolls. 
-twoDiePermutations  = list(itertools.product(dice, repeat = n))
+#creating the Sample Space of the 2 die by taking the cartisian product.
+sampleSpace  = list(itertools.product(dice, repeat = n))
 
-
+desiredSum = int(input("The odds of rolling a: "))
 print("These are all the posible permutations of two 6 sided dice.")
-print(twoDiePermutations)
+print(sampleSpace)
 
-#list var that will hold all possible permutations of 2  dice that sum to the desired sum
-desiredSumPermutations = [] 
+#this is a list that will holds the desired subset from the Sample Space.
+event = [] 
 
-#This for look will iterate over all the permutations, sum the first element of the tuple with the second, if equal to desired sum, it will appened it to the desiredSumPermutations list. 
-for x in range (0,len(twoDiePermutations)):
-    if twoDiePermutations[x][0] + twoDiePermutations[x][1]  == desiredSum:
-        desiredSumPermutations.append(twoDiePermutations[x])
+#This for loop will iterate over the Sample Space, sum the first element of the tuple with the second, if equal to desired sum, it will appened it to the event list. 
+for x in range (0,len(sampleSpace)):
+    if sampleSpace[x][0] + sampleSpace[x][1]  == desiredSum:
+        event.append(sampleSpace[x])
 
 
-print("These are all the possible permuations of two 6 sided dice that will sum to " + str(desiredSum))
-print(desiredSumPermutations)
+print("These are all the possible permuations of two six sided dice that will sum to " + str(desiredSum))
+print(event)
 
-probability = len(desiredSumPermutations)/len(twoDiePermutations)
+#Laplace's definition for the probability of an event.
+probability = len(event)/len(sampleSpace)
 
 print("The probablity of rolling a "+ str(desiredSum) +" with two six-sided dice is: " + format(probability,'.4f') + "%")
