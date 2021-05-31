220e729ccd35beb637d7f2c5d8e3d0f8f5270642
andrewp2303 <65823271+andrewp2303@users.noreply.github.com>
2021-05-30 20:18:36 -0700

    updated pigeon.java

--- Andrew Palacci/pigeon.java
+++ Andrew Palacci/pigeon.java
@@ -1,110 +1,64 @@
 /* 
-This program demonstrates the Generalized Pigeonhole principle by taking a list of the most popular male names in the United States
-from each first letter, forming a list of 26 names, and then randomly sorting these names into five distinct lists, named List1 - List5.
-By the Generalized Pigeonhole principle, one of these lists must contain ⌈26/5⌉ = 6 names in it. The program's output confirms this by
-displaying the names contained within each list and then displaying the lists with at least 6 names, as well as the exact amount of names
-those lists contain.
+This program demonstrates the Pigeonhole principle by generating a list of 11 random one-digit integers [0, 9], then showing that 
+there exists at least one such integer that is repeated at least twice. The program's output shows all of the repeated numbers and
+the frequency by which they were repeated, in the form [number] ([frequency]x). Each number is separated by a double space.
 
-If one were to run this program infinitely many times, it would never return a blank output (meaning that there were no lists containing
-6 or more names). This can also be proven through the Generalized Pigeonhole principle because we are sorting 26 random names into 
-5 lists (ordered 1-5), so at least one lists must have 6 or more names in it.
+If one were to run this program infinitely many times, it would never return a blank output (meaning that there were no repeats). This 
+can be proven through the pigeonhole principle because we are sorting 11 random integers into 10 boxes (ordered 0-9), so at least one
+box must have 2 or more objects (randomly chosen integers) inside of it.
 */
 
 import java.util.*;
 import java.math.*;
 
-public class genPigeon
+public class Pigeon
 {
   public static void main (String[] args)
-  { 
-    //initializes String array containing the 26 names, num variable for randomization, len for names.length, and the 5 ArrayLists
-    String names[] = {"Alexander", "Benjamin", "Carter", "Daniel", "Elijah", "Finn", "Grayson", "Henry", "Isaac", 
-    "James", "Kayden", "Liam", "Mason", "Noah", "Oliver", "Parker", "Quinn", "Ryan", "Sebastian", "Theodore", 
-    "Uriel", "Vincent", "William", "Xavier", "Yusuf", "Zachary"};
-    int num;
-    int len = names.length;
-    ArrayList<String> List1 = new ArrayList<String>();
-    ArrayList<String> List2 = new ArrayList<String>();
-    ArrayList<String> List3 = new ArrayList<String>();
-    ArrayList<String> List4 = new ArrayList<String>();
-    ArrayList<String> List5 = new ArrayList<String>();
-
-    //loops through all names in names[] and sorts them into the 5 ArrayLists
-    for (int i = 0; i < len; i++)
-    {
-        num = (int)Math.ceil(Math.random() * 5);
-        if (num == 1)
-        {
-            List1.add(names[i]);
-        } else if (num == 2)
-        {
-            List2.add(names[i]);
-        } else if (num == 3)
-        {
-            List3.add(names[i]);
-        } else if (num == 4)
-        {
-            List4.add(names[i]);
-        } else
-        {
-            List5.add(names[i]);
-        }
-    }
-
+  {
     //welcome message
-    System.out.println("\nWelcome to the Generalized Pigeonhole Principle Demonstration!\n");
+    System.out.println("\nWelcome to the Pigeonhole Principle Demonstration!\n");
 
-    //prints the contents of each list
-    System.out.print("List 1 names: ");
-    for (String name : List1)
-    {
-        System.out.print(name + " ");
-    }
-    System.out.print("\nList 2 names: ");
-    for (String name : List2)
-    {
-        System.out.print(name + " ");
-    }
-    System.out.print("\nList 3 names: ");
-    for (String name : List3)
-    {
-        System.out.print(name + " ");
-    }
-    System.out.print("\nList 4 names: ");
-    for (String name : List4)
-    {
-        System.out.print(name + " ");
-    }
-    System.out.print("\nList 5 names: ");
-    for (String name : List5)
-    {
-        System.out.print(name + " ");
-    }
+    //initializes an array that will contain the random integers
+    int[] nums = new int[11];
 
-    //finds lists with over 5 names and prints the list number and quantity of names
-    System.out.println("\n\nLists with more than 5 names: ");
-    if (List1.size() > 5)
+    //adds random numbers to the array nums and prints out the array's contents
+    System.out.print("Test Numbers: ");
+    for (int i = 0; i < 10; i++)
     {
-        System.out.print("List 1 (" + List1.size() + ")  ");
+        nums[i] = (int)Math.floor(Math.random() * 10);
+        System.out.print(nums[i] + ", ");
     }
-    if (List2.size() > 5)
-    {
-        System.out.print("List 2 (" + List2.size() + ")  ");
-    }
-    if (List3.size() > 5)
-    {
-        System.out.print("List 3 (" + List3.size() + ")  ");
-    }
-    if (List4.size() > 5)
-    {
-        System.out.print("List 4 (" + List4.size() + ")  ");
-    }
-    if (List5.size() > 5)
-    {
-        System.out.print("List 5 (" + List5.size() + ")  ");
-    }
+    System.out.println(nums[10]);
+
+    //creates instance of Pigeon class and uses it to print out repeated numbers
+    Pigeon tester = new Pigeon();
+    tester.printRepeats(nums);
 
     //thank you message
     System.out.println("\n\nThank you for coming to this demonstration!\n");
+  }
+
+  public void printRepeats(int[] numbers)
+  {
+        //initializes a variable to store the length of the original array, then initializes an array to store repeat counts
+        int len = numbers.length;
+        int[] repeats = new int[10];
+        
+        //loops through the original array's numbers and adds 1 to repeats for each occurrence of an integer at that integer's index
+        for (int i = 0; i < len; i++)
+        {
+            repeats[numbers[i]]++;
+        }
+
+        //loops through both arrays to output which numbers were repeated and their frequencies
+        System.out.print("Repeated numbers: ");
+        for (int j = 0; j < 10; j++)
+        {
+            if (repeats[numbers[j]] > 1)
+            {
+                System.out.print(numbers[j] + " (" + repeats[numbers[j]] + "x)  ");
+                repeats[numbers[j]] = 1;
+            }
+        }
   }
 }
